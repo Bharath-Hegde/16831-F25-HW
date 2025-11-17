19acf6cf6fde1247b60216128d906053df33b055
Bharath <bharath.hegde101@gmail.com>
2025-11-17 22:35:37 +0000

    problem 6 todos

--- hw4/rob831/hw4_part2/exploration/rnd_model.py
+++ hw4/rob831/hw4_part2/exploration/rnd_model.py
@@ -22,14 +22,41 @@ class RNDModel(nn.Module, BaseExplorationModel):
         self.size = hparams['rnd_size']
         self.optimizer_spec = optimizer_spec
 
-        # <TODO>: Create two neural networks:
+        # <TODO>: Create two neural networks: - done
         # 1) f, the random function we are trying to learn
         # 2) f_hat, the function we are using to learn f
 
+        self.f = ptu.build_mlp(
+            input_size=self.ob_dim,
+            output_size=self.output_size,
+            n_layers=self.n_layers,
+            size=self.size
+        )
+        self.f.apply(lambda m: init_method_2(m) if isinstance(m, nn.Linear) else None)
+        self.f.to(ptu.device)
+
+        self.f_hat = ptu.build_mlp(
+            input_size=self.ob_dim,
+            output_size=self.output_size,
+            n_layers=self.n_layers,
+            size=self.size
+        )
+        self.f_hat.apply(lambda m: init_method_1(m) if isinstance(m, nn.Linear) else None)
+        self.f_hat.to(ptu.device)
+
+        self.optimizer = self.optimizer_spec.constructor(
+            self.f_hat.parameters(),
+            **self.optimizer_spec.optim_kwargs
+        )
+
     def forward(self, ob_no):
-        # <TODO>: Get the prediction error for ob_no
+        # <TODO>: Get the prediction error for ob_no - done
         # HINT: Remember to detach the output of self.f!
-        pass
+        target = self.f(ob_no).detach()
+        prediction = self.f_hat(ob_no)
+        error = torch.norm(prediction - target, dim=1, p=2)
+
+        return error
 
     def forward_np(self, ob_no):
         ob_no = ptu.from_numpy(ob_no)
@@ -37,6 +64,15 @@ class RNDModel(nn.Module, BaseExplorationModel):
         return ptu.to_numpy(error)
 
     def update(self, ob_no):
-        # <TODO>: Update f_hat using ob_no
+        # <TODO>: Update f_hat using ob_no - done
         # Hint: Take the mean prediction error across the batch
-        pass
+        ob_no = ptu.from_numpy(ob_no)
+
+        error = self.forward(ob_no)
+        loss = error.mean()
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return ptu.to_numpy(loss)
